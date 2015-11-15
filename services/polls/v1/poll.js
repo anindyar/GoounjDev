d3506375f9731de0c255b9305866d1f37be12fcd
Catherine Samuel <catherine.orgware@gmail.com>
2015-11-15 08:40:11 +0530

    updated poll

--- services/polls/v1/poll.js
+++ services/polls/v1/poll.js
@@ -79,6 +79,7 @@ var pushNote = require('./../../../push');
  *    "visibilityType": "visible",
  *    "rewardType": "free",
  *    "category": "sports",
+ *    "isGeneric": "0",
  *    "createdUserId": "1",
  *    "pollType": "opinion",
  *    "questionList": [
@@ -134,7 +135,7 @@ exports.create = function(request, response) {
                     var dat = new Date().setDate(new Date().getDate() + config.poll.expiresAfter);
                     var utcTimeStampEnd = moment(dat).format('YYYY/MM/DD HH:mm:ss');
 
-                    connection.query('INSERT INTO ' + config.mysql.db.name +'.poll (start_date, end_date, poll_name, is_boost, visibility_type_id, reward_type_id, created_user_id, poll_type_id, is_active, is_generic) VALUES (?, ?, ?, ?, (SELECT id FROM visibility_type WHERE type=?), (SELECT id FROM reward_type WHERE type=?), ?, (SELECT id FROM poll_type WHERE type=?), ?, ?)', [utcTimeStamp, utcTimeStampEnd, request.body.pollName, request.body.isBoost, request.body.visibilityType, request.body.rewardType, request.body.createdUserId, request.body.pollType, "1", request.body.isGeneric], function (queryError, poll) {
+                    connection.query('INSERT INTO ' + config.mysql.db.name +'.poll (start_date, end_date, poll_name, is_boost, visibility_type_id, reward_type_id, created_user_id, poll_type_id, is_active, is_generic) VALUES (?, ?, ?, ?, (SELECT id FROM visibility_type WHERE type=?), (SELECT id FROM reward_type WHERE type=?), ?, (SELECT id FROM poll_type WHERE type=?), ?, ?)', [utcTimeStamp, utcTimeStampEnd, request.body.pollName, request.body.isBoost, request.body.visibilityType, request.body.rewardType, request.body.createdUserId, request.body.pollType, request.body.isGeneric, request.body.isGeneric], function (queryError, poll) {
                         if (queryError != null) {
                             log.error(queryError, "Query error. Failed to create a new poll. User details " + JSON.stringify(request.body.phone) + "(Function= Poll Create)");
                             json = {
