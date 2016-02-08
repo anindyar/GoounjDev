cbd452f676c27df47eb27d5b880825d948026a9d
Catherine Samuel <catherine.orgware@gmail.com>
2016-02-08 18:22:39 +0530

    updated sp_setAudienceForSurvey and answerSurvey

--- db/sp_setAudienceForSurvey.sql
+++ db/sp_setAudienceForSurvey.sql
@@ -1,9 +1,9 @@
 USE `goounj`;
 DROP procedure IF EXISTS `setAudienceForSurvey`;
 
-DELIMITER //
-USE `goounj`//
-CREATE PROCEDURE `setAudienceForSurvey`(IN phoneNumber VARCHAR(45), IN pollId VARCHAR(45), IN utcTimeStamp DATETIME, IN name VARCHAR(45), OUT userId INT(11))
+DELIMITER $$
+USE `goounj`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `setAudienceForSurvey`(IN phoneNumber VARCHAR(45), IN pollId VARCHAR(45), IN utcTimeStamp DATETIME, IN name VARCHAR(45), OUT userId INT(11), OUT deviceToken VARCHAR(200))
     DETERMINISTIC
     COMMENT 'Procedure to assign audience to a survey'
 BEGIN
@@ -12,10 +12,10 @@ IF userId IS NULL THEN
     INSERT IGNORE INTO user (phone, role_id, auth_type_id) VALUES (phoneNumber, '1', '1');
     SET userId := LAST_INSERT_ID(); -- LAST_INSERT_ID() can give you the real, surrogate key
 END IF;
+SET deviceToken := (SELECT device_token FROM user WHERE phone = phoneNumber LIMIT 1);
 UPDATE user SET name = name WHERE id = userId;
 INSERT INTO audience_poll_map (user_id, poll_id, poll_answered_time, is_answered) VALUES (userId, pollId, utcTimeStamp, 1);
-
-END//
+END$$
 
 DELIMITER ;
 
--- services/survey/v1/answerSurvey.js
+++ services/survey/v1/answerSurvey.js
@@ -29,6 +29,8 @@
 var config = require('./../../../config');
 var log = require('./../../../log');
 var moment = require('moment');
+var sms = require('./../../../sms');
+var pushNote = require('./../../../push');
 
 
 
@@ -102,37 +104,75 @@ exports.create = function(request, response) {
                     };
                     return response.status(500).json(json);
                 }
-                var utcTimeStamp = moment(new Date()).format('YYYY/MM/DD HH:mm:ss');
-                var name = request.body.name;
-                connection.query('SET @userId = 0; CALL setAudienceForSurvey(?, ?, ?, ?, @userId); SELECT @userId AS userId;', [request.body.phone, request.body.pollId, utcTimeStamp, name], function (queryError, user) {
+                connection.query('SELECT * FROM '+ config.mysql.db.name +'.audience_poll_map WHERE user_id = (SELECT id FROM '+ config.mysql.db.name +'.user WHERE phone = ?) AND poll_id = ?', [request.body.phone, request.body.pollId], function(queryError, check) {
                     if (queryError != null) {
-                        log.error(queryError, "Query error. Failed to update audience. User details " + JSON.stringify(request.body.phone) + "(Function= AnswerSurvey.Create)");
-                        jsn = {
+                        log.error(queryError, "Query error. Failed to record a new survey answer. Answer details " + JSON.stringify(request.body.pollId) + "(Function = AnswerSurvey.Create)");
+                        json = {
                             error: "Requested action failed. Database could not be reached."
                         };
                         return response.status(500).json(json);
                     }
-                    else if(user) {
-                        var userObj = user[2];
-                        var userId = userObj[0].userId;
-                        var questionAnswer = request.body.questionList;
-                        for(var i=0; i<questionAnswer.length; i++) {
-                            connection.query('INSERT INTO ' + config.mysql.db.name + '.answer (time, question_id, question_options_id, user_id, poll_id) VALUES (?, ?, ?, ?, ?)', [utcTimeStamp, questionAnswer[i].questionId, questionAnswer[i].optionId, userId, request.body.pollId], function (queryError, result) {
-                                if (queryError != null) {
-                                    log.error(queryError, "Query error. Failed to record a new survey answer. Answer details " + JSON.stringify(request.body.pollId) + "(Function = AnswerSurvey.Create)");
-                                    json = {
-                                        error: "Requested action failed. Database could not be reached."
-                                    };
-                                    return response.status(500).json(json);
-                                }
-                            });
-                        }
-                        log.info({Function: "AnswerSurvey.Create"}, "New survey answer has been recorded successfully.");
-                        return response.sendStatus(200);
+                    if(check.length != 0) {
+                        json = {
+                            error: "User has already answered this survey."
+                        };
+                        log.info({Function: "AnswerSurvey.Create"}, "User already answered this survey.");
+                        return response.status(400).json(json);
                     }
                     else {
-                        log.info({Function: "AnswerSurvey.Create"}, "Requested Survey Not Found");
-                        return response.sendStatus(404);
+                        var utcTimeStamp = moment(new Date()).format('YYYY/MM/DD HH:mm:ss');
+                        var name = request.body.name;
+                        connection.query('SET @userId = 0, @deviceToken = ""; CALL setAudienceForSurvey(?, ?, ?, ?, @userId, @deviceToken); SELECT @userId AS userId, @deviceToken AS deviceToken;', [request.body.phone, request.body.pollId, utcTimeStamp, name], function (queryError, user) {
+                            if (queryError != null) {
+                                log.error(queryError, "Query error. Failed to update audience. User details " + JSON.stringify(request.body.phone) + "(Function= AnswerSurvey.Create)");
+                                jsn = {
+                                    error: "Requested action failed. Database could not be reached."
+                                };
+                                return response.status(500).json(json);
+                            }
+                            else if(user) {
+                                var pollName = "";
+                                var userObj = user[2];
+                                var userId = userObj[0].userId;
+                                var deviceToken = userObj[0].deviceToken;
+                                var questionAnswer = request.body.questionList;
+                                for(var i=0; i<questionAnswer.length; i++) {
+                                    connection.query('INSERT INTO ' + config.mysql.db.name + '.answer (time, question_id, question_options_id, user_id, poll_id) VALUES (?, ?, ?, ?, ?); SELECT poll_name FROM ' + config.mysql.db.name + '.poll WHERE id = ?', [utcTimeStamp, questionAnswer[i].questionId, questionAnswer[i].optionId, userId, request.body.pollId, request.body.pollId], function (queryError, result) {
+                                        if (queryError != null) {
+                                            log.error(queryError, "Query error. Failed to record a new survey answer. Answer details " + JSON.stringify(request.body.pollId) + "(Function = AnswerSurvey.Create)");
+                                            json = {
+                                                error: "Requested action failed. Database could not be reached."
+                                            };
+                                            return response.status(500).json(json);
+                                        }
+                                        else {
+                                            var pollObj = result[1];
+                                            pollName = pollObj[1].poll_name;
+                                        }
+                                    });
+                                }
+                                if(deviceToken == null) {
+                                    if(config.sms.enabled) {
+                                        var phoneArray = [];
+                                        phoneArray.push(request.body.phone);
+                                        sms.sendSMS(phoneArray, "This is Goounj Service. You have answered a survey: "+ pollName +"! Thank you for your support.");
+                                    }
+                                }
+                                else {
+                                    if(config.pushNotification.enabled) {
+                                        var tokenArray = [];
+                                        tokenArray.push(deviceToken);
+                                        pushNote.sendAndroidPush(tokenArray, "You have answered a survey: " + pollName + "!");
+                                    }
+                                }
+                                log.info({Function: "AnswerSurvey.Create"}, "New survey answer has been recorded successfully.");
+                                return response.sendStatus(200);
+                            }
+                            else {
+                                log.info({Function: "AnswerSurvey.Create"}, "Requested Survey Not Found");
+                                return response.sendStatus(404);
+                            }
+                        });
                     }
                 });
             });
