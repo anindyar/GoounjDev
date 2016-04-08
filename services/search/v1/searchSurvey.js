f6605b3aa673a57c753ea93732927518015f2695
Catherine Samuel <catherine.orgware@gmail.com>
2016-04-08 12:34:38 +0530

    updated date time format

--- services/search/v1/searchSurvey.js
+++ services/search/v1/searchSurvey.js
@@ -126,7 +126,7 @@ exports.create = function(request, response){
                     };
                     return response.status(500).json(json);
                 }
-                var utcTimeStamp = moment(new Date()).format('YYYY/MM/DD HH:mm:ss');
+                var utcTimeStamp = moment(new Date()).format('DD/MM/YYYY');
                 connection.query('SELECT poll.id AS pollId, start_date AS startDate, end_date AS endDate, poll_name AS pollName, is_survey AS isSurvey, is_boost AS isBoost, poll.is_active AS isActive, is_generic AS isGeneric, created_user_id AS createdUserId, name AS createdUserName  FROM poll INNER JOIN user ON poll.created_user_id = user.id WHERE poll.end_date > ? AND is_survey = 1 AND poll_name LIKE ? ORDER BY pollId DESC', [utcTimeStamp, '%' + request.body.searchString + '%'], function(queryError, result) {
                     if (queryError != null) {
                         log.error(queryError, "Query error. Failed to fetch survey list. Details " + JSON.stringify(request.body.userId) + "(Function = searchSurvey.Create)");
