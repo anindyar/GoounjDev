fea4f959e8946441352dc587c9a9333edc6aa975
Catherine Samuel <catherine.orgware@gmail.com>
2016-05-31 14:49:33 +0530

    added options to election list by association

--- services/vote/v1/electionListByAssociation.js
+++ services/vote/v1/electionListByAssociation.js
@@ -19,7 +19,7 @@ exports.show = function(request, response) {
             }
             connection.query('SELECT election.id AS electionId, election.name AS electionName, created_date AS createdDate, start_date AS startDate, end_date AS endDate, nomination_end_date AS nominationEndDate, vigilance_user_id AS vigilanceUserId, (SELECT name FROM user WHERE id = election.vigilance_user_id) AS vigilanceUser, association_id AS associationId, (SELECT name FROM association WHERE id = election.association_id) AS associationName, (SELECT COUNT(vote.id) FROM vote JOIN election ON election.id = vote.election_id WHERE election.id = electionId GROUP BY election.id) AS voteCount, (SELECT COUNT(id) FROM election_user_map WHERE association_id = ?) AS eligibleToVote  FROM election JOIN association ON association.id = election.association_id WHERE association.id = ? ORDER BY election.id DESC', [request.params.id, request.params.id], function (queryError, list) {
                 if (queryError != null) {
-                    log.error(queryError, "Query error. Failed to fetch election list. Details " + JSON.stringify(request.body.userId) + "(Function = ElectionList.Show)");
+                    log.error(queryError, "Query error. Failed to fetch election list. Details " + JSON.stringify(request.body.userId) + "(Function = ElectionListByAssociation.Show)");
                     json = {
                         error: "Requested action failed. Database could not be reached."
                     };
@@ -36,7 +36,7 @@ exports.show = function(request, response) {
                             var kCopy = k;
                             connection.query('SELECT candidate.id AS candidateId, candidate.name AS candidate, COUNT(candidate_id) AS votes FROM election INNER JOIN candidate ON candidate.election_id = election.id RIGHT JOIN vote ON (vote.candidate_id = candidate.id) WHERE election.id = ? GROUP BY vote.candidate_id;', list[kCopy].electionId, function(queryError, resultSet) {
                                 if (queryError != null) {
-                                    log.error(queryError, "Query error. Failed fetch vote results. Vote Result details " + JSON.stringify(request.params.id) + "(Function = VoteResult.Show)");
+                                    log.error(queryError, "Query error. Failed fetch vote results. (Function = ElectionListByAssociation.Show)");
                                     json = {
                                         error: "Requested action failed. Database could not be reached."
                                     };
@@ -117,4 +117,8 @@ exports.show = function(request, response) {
         log.error(error, "Exception Occurred (Function = ElectionListByAssociation.Show)");
         return response.status(500).json(json);
     }
+};
+
+exports.options = function(request, response) {
+    return response.sendStatus(200);
 };
